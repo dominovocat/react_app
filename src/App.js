4bac7634a81a921c71f183ae6e9afd66f28e346c
danil <minakonn9shkoni@gmail.com>
2021-12-08 10:54:48 +0300

    Add theme switch

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import './App.css';
-import React, {useState} from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import {UserContext} from './context';
+import "./App.css";
+import React, { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import { UserContext, ThemeContext } from "./context";
+import CONSTANTS from './constants';
+const {THEMES} = CONSTANTS;
 
-function App(){
+function App() {
   const [user, setUser] = useState({
-    id:1,
-    name:'Elon Musk'
+    id: 1,
+    name: "Elon Musk",
   });
-  return(
+  const themeState = useState(THEMES.DARK);
+  
+  return (
+    <ThemeContext.Provider value={themeState}>
     <UserContext.Provider value={user}>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
     </UserContext.Provider>
+    </ThemeContext.Provider>
   );
 }
 
 export default App;
-
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import UserProfile from '../components/UserProfile';
+import { ThemeContext } from '../context';
+import CONSTANTS from '../constants';
+const {THEMES} = CONSTANTS;
+  const styleMap = {
+    [THEMES.LIGHT]:{
+      background:'#eee',color:'#333',height:'100vh'
+    },
+    [THEMES.DARK]:{
+      background:'#333',color:'#eee',height:'100vh'
+    }
+  }
 
 const HomePage = () => {
+  const [theme, themeState] = useContext(ThemeContext);
+  const handlerTheme = ()=>{
+    themeState(theme===THEMES.DARK?THEMES.LIGHT:THEMES.DARK);
+  };
   return (
-    <div>
+    <div style={styleMap[theme]}>
+      <button onClick={handlerTheme} >switch</button>
       <UserProfile />
     </div>
   );

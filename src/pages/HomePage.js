e60709f778ad7307a4cf0dde4cb726a659597d8e
danil <minakonn9shkoni@gmail.com>
2021-12-08 12:00:27 +0300

    Add useMemo

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,37 +1,50 @@
-import React, {useContext, useState, useCallback, useEffect} from 'react';
-import UserProfile from '../components/UserProfile';
-import { ThemeContext } from '../context';
-import CONSTANTS from '../constants';
-const {THEMES} = CONSTANTS;
-  const styleMap = {
-    [THEMES.LIGHT]:{
-      background:'#eee',color:'#333',height:'100vh'
-    },
-    [THEMES.DARK]:{
-      background:'#333',color:'#eee',height:'100vh'
-    }
-  }
+import React, { useContext, useState, useCallback, useEffect, useMemo} from "react";
+import UserProfile from "../components/UserProfile";
+import { ThemeContext } from "../context";
+import CONSTANTS from "../constants";
+const { THEMES } = CONSTANTS;
+
+function calcValue(n){
+  let res = 0
+  for(let i=0;i<1000000;i++){res+=i}
+  
+  return n**5};
+
+const styleMap = {
+  [THEMES.LIGHT]: {
+    background: "#eee",
+    color: "#333",
+    height: "100vh",
+  },
+  [THEMES.DARK]: {
+    background: "#333",
+    color: "#eee",
+    height: "100vh",
+  },
+};
 
 const HomePage = () => {
   const [value, setValue] = useState(0);
   const [theme, themeState] = useContext(ThemeContext);
-  const handlerTheme = useCallback(()=>{
-    themeState(theme===THEMES.DARK?THEMES.LIGHT:THEMES.DARK);
-  },[theme]);  
-  const handlerLog = useCallback(()=>{console.log(value)},[value]);
-  const handlerInput = useCallback(({target:{value}})=>{
-    setValue(Number(value))
-  },[])
-
+  const handlerTheme = useCallback(() => {
+    themeState(theme === THEMES.DARK ? THEMES.LIGHT : THEMES.DARK);
+  }, [theme]);
+  const handlerLog = useCallback(() => {
+    console.log(value);
+  }, [value]);
+  const handlerInput = useCallback(({ target: { value } }) => {
+    setValue(Number(value));
+  }, []);
+  const showCalcValue = useMemo(()=>calcValue(value),[value])
   return (
     <div style={styleMap[theme]}>
-      <h2>Value:{value}</h2>
-      <input type='number' value={value} onChange={handlerInput}/>
-      <button onClick={handlerTheme} >switch</button>
-      <button onClick={handlerLog} >log value</button>
+      <h2>Value:{showCalcValue}</h2>
+      <input type="number" value={value} onChange={handlerInput} />
+      <button onClick={handlerTheme}>switch</button>
+      <button onClick={handlerLog}>log value</button>
       <UserProfile />
     </div>
   );
-}
+};
 
 export default HomePage;

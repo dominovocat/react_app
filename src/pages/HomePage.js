84b59bdfac75e430323fd26923727ad1116a4672
danil <minakonn9shkoni@gmail.com>
2021-12-08 11:29:26 +0300

    Add useCallback

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState, useCallback, useEffect} from 'react';
 import UserProfile from '../components/UserProfile';
 import { ThemeContext } from '../context';
 import CONSTANTS from '../constants';
@@ -13,13 +13,22 @@ const {THEMES} = CONSTANTS;
   }
 
 const HomePage = () => {
+  const [value, setValue] = useState(0);
   const [theme, themeState] = useContext(ThemeContext);
-  const handlerTheme = ()=>{
+  const handlerTheme = useCallback(()=>{
     themeState(theme===THEMES.DARK?THEMES.LIGHT:THEMES.DARK);
-  };
+  },[theme]);  
+  const handlerLog = useCallback(()=>{console.log(value)},[value]);
+  const handlerInput = useCallback(({target:{value}})=>{
+    setValue(Number(value))
+  },[])
+
   return (
     <div style={styleMap[theme]}>
+      <h2>Value:{value}</h2>
+      <input type='number' value={value} onChange={handlerInput}/>
       <button onClick={handlerTheme} >switch</button>
+      <button onClick={handlerLog} >log value</button>
       <UserProfile />
     </div>
   );
